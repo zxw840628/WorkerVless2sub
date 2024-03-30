dcc7d74a5480893ea64fce5c1d0ababf221ba2c6
zxw840628 <48180246+zxw840628@users.noreply.github.com>
2024-03-31 07:28:41 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,13 +5,16 @@ let mytoken= 'auto';//快速订阅访问入口, 留空则不启动快速订阅
 
 // 设置优选地址，不带端口号默认443，TLS订阅生成
 let addresses = [
-	//'icook.tw:2053#官方优选域名',
-	//'cloudflare.cfgo.cc#优选官方线路',
+	'icook.tw:2053#官方优选域名',
+	'cloudflare.cfgo.cc#优选官方线路',
+	'cf.090227.xyz:2087#官方优选IP-CM提供',
+	'cfip.xxxxxxxx.tk:8443#官方优选IP-OTC提供',
 ];
 
 // 设置优选地址api接口
 let addressesapi = [
-	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
+	'https://addressesapi.090227.xyz/CloudFlareYes',
+	'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
 ];
 
